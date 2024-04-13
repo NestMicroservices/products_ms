85597c40896c917459f6a05e3643e772a73165a8
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-04-12 21:38:04 -0500

    nats env

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -17,7 +17,7 @@ const envsSchema = joi
 
 const { error, value } = envsSchema.validate({
   ...process.env,
-  NATS_SERVERS: process.env.NATS_SERVERS.split(','),
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
 });
 
 if (error) {
